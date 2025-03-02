10ec35cb5c49d9141aa0e8b5695058f9a8be89d4
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-03-02 21:59:26 +0500

    feat: store resources locally

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,21 +1,23 @@
-import { db } from "@/db";
-import { resourcesTable } from "@/schema";
+import data from "./resources.json";
+// import { db } from "@/db";
+// import { resourcesTable } from "@/schema";
 
 export default async function Page() {
-  const resources = await db.select().from(resourcesTable);
+  console.log(data);
+  // const resources = await db.select().from(resourcesTable);
 
   return (
     <div>
       <h1 className="text-4xl font-extrabold">Resources</h1>
 
       <div className="my-6 w-full grid grid-cols-1 gap-4 sm:grid-cols-2">
-        {resources.map((resource) => (
+        {data.map((resource, id) => (
           <a
-            key={resource.id}
-            href={resource.link}
+            key={id}
             className="p-3 border rounded-lg w-full"
+            href={resource.url}
           >
-            {resource.title}
+            <h3>{resource.name}</h3>
           </a>
         ))}
       </div>
