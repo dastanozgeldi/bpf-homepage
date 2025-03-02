193313a40f45d0fd1fa07913a64cd09083210535
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-03-02 22:01:07 +0500

    feat: add link to navigate home

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import data from "./resources.json";
 // import { db } from "@/db";
 // import { resourcesTable } from "@/schema";
@@ -7,7 +8,10 @@ export default async function Page() {
 
   return (
     <div>
-      <h1 className="text-4xl font-extrabold">Resources</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-extrabold">Resources</h1>
+        <Link href="/">go home</Link>
+      </div>
 
       <div className="my-6 w-full grid grid-cols-1 gap-4 sm:grid-cols-2">
         {data.map((resource, id) => (
