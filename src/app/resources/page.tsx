b97cb57f6de5f63557b2e6a675da2a4049a4b058
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-03-03 22:44:43 +0500

    feat: comment unnecessary db call

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,17 +1,14 @@
-import Link from "next/link";
-
 import data from "./resources.json" assert { type: "json" };
-import { db } from "@/db";
-import { resourcesTable } from "@/schema";
+// import { db } from "@/db";
+// import { resourcesTable } from "@/schema";
 
 export default async function Page() {
-  const resources = await db.select().from(resourcesTable);
-  console.log(resources)
+  // const resources = await db.select().from(resourcesTable);
+  // console.log(resources);
   return (
     <div>
       <div className="flex items-center justify-between">
         <h1 className="text-4xl font-extrabold">Resources</h1>
-        <Link href="/">go home</Link>
       </div>
 
       <div className="my-6 w-full grid grid-cols-1 gap-4 sm:grid-cols-2">
